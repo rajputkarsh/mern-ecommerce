b41ed770cab2e1466ca223b7f856484de0aa6f5d
Utkarsh <r.utkarsh.0010@gmail.com>
2022-03-29 05:54:07 +0530

    Added Product related pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product"
 
 
 const App = () => {
-  return <Home/>
+  return <Product />
+  // return <ProductList />
+  // return <Home/>
 };
 
 export default App;
